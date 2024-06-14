06e2531654db7781209f423a8f6631ad78fe4e7d
Filipe Plucenio <plucenio@gmail.com>
2024-06-14 02:33:21 -0300

    implemented a new test.

--- bin/dart_application_test.dart
+++ bin/dart_application_test.dart
@@ -1,26 +1,26 @@
 import 'dart:math';
 
 void main(List<String> arguments) {
-  execute([1, 0, 1, 3]);
+  trap([1, 0, 1, 3]);
 }
 
-int execute(List<int> arguments) {
+int trap(List<int> height) {
   int higher = 0;
   int consolidatedWater = 0;
-  for (int i = 1; i < arguments.length; i++) {
-    higher = max(arguments[i - 1], higher);
-    if (i + 1 < arguments.length && arguments[i + 1] > arguments[i]) {
+  for (int i = 1; i < height.length; i++) {
+    higher = max(height[i - 1], higher);
+    if (i + 1 < height.length && height[i + 1] > height[i]) {
       continue;
     }
-    if (arguments[i] > arguments[i - 1]) {
-      int lower = min(higher, arguments[i]);
+    if (height[i] > height[i - 1]) {
+      int lower = min(higher, height[i]);
       int water = 0;
       for (int j = i - 1; j >= 0; j--) {
-        if (lower > arguments[j]) {
-          water = water + (lower - arguments[j]);
+        if (lower > height[j]) {
+          water = water + (lower - height[j]);
           continue;
         }
-        if (arguments[j + 1] > arguments[j]) {
+        if (height[j + 1] > height[j]) {
           continue;
         }
         consolidatedWater = consolidatedWater + water;
--- test/dart_application_test_test.dart
+++ test/dart_application_test_test.dart
@@ -4,62 +4,62 @@ import '../bin/dart_application_test.dart';
 
 void main() {
   test('Test 1', () {
-    expect(execute([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), equals(6));
+    expect(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), equals(6));
   });
 
   test('Test 2', () {
-    expect(execute([10, 5, 0, 5]), equals(5));
+    expect(trap([10, 5, 0, 5]), equals(5));
   });
 
   test('Test 3', () {
-    expect(execute([0, 1, 0, 2]), equals(1));
+    expect(trap([0, 1, 0, 2]), equals(1));
   });
 
   test('Test 4', () {
-    expect(execute([3, 2, 1, 0, 1]), equals(1));
+    expect(trap([3, 2, 1, 0, 1]), equals(1));
   });
 
   test('Test 5', () {
-    expect(execute([2, 1, 2, 1]), equals(1));
+    expect(trap([2, 1, 2, 1]), equals(1));
   });
 
   test('Test 6', () {
-    expect(execute([2, 1, 0, 1, 3]), equals(4));
+    expect(trap([2, 1, 0, 1, 3]), equals(4));
   });
 
   test('Test 7', () {
-    expect(execute([1, 0, 1, 3]), equals(1));
+    expect(trap([1, 0, 1, 3]), equals(1));
   });
 
   test('Test 8', () {
-    expect(execute([1, 0, 1, 3, 2, 1, 2, 1]), equals(2));
+    expect(trap([1, 0, 1, 3, 2, 1, 2, 1]), equals(2));
   });
 
   test('Test 9', () {
-    expect(execute([1, 0, 1, 0, 1]), equals(2));
+    expect(trap([1, 0, 1, 0, 1]), equals(2));
   });
 
   test('Test 10', () {
-    expect(execute([]), equals(0));
+    expect(trap([]), equals(0));
   });
 
   test('Test 11', () {
-    expect(execute([1]), equals(0));
+    expect(trap([1]), equals(0));
   });
 
   test('Test 12', () {
-    expect(execute([0, 2, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), equals(7));
+    expect(trap([0, 2, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), equals(7));
   });
 
   test('Test 13', () {
-    expect(execute([0, 2, 0, 2, 1, 0, 1, 3, 2, 1, 2, 4]), equals(10));
+    expect(trap([0, 2, 0, 2, 1, 0, 1, 3, 2, 1, 2, 4]), equals(10));
   });
 
   test('Test 14', () {
-    expect(execute([1, ...List.generate(998, (index) => 0), 1]), equals(998));
+    expect(trap([1, ...List.generate(998, (index) => 0), 1]), equals(998));
   });
 
   test('Test 15', () {
-    expect(execute([4, 2, 0, 3, 2, 5]), equals(9));
+    expect(trap([4, 2, 0, 3, 2, 5]), equals(9));
   });
 }
